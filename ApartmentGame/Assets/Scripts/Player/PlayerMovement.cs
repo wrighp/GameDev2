c0a197b3dfc440dbea915e2f0410f2b413829fe1
wrighp <wrightCoder@gmail.com>
2017-02-09 21:28:57 -0500

    NPC Pathfinding tests and basic Player Movement
    Click on the world in hte NavigationTest scene to make them go places.

---
+++ ApartmentGame/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Player movement, direction based off of camera rotation
+/// </summary>
+[RequireComponent (typeof (Rigidbody))]
+public class PlayerMovement : MonoBehaviour {
+
+	public Camera camera;
+	public float acceleration;
+	public float maxSpeed; //Max horizontal speed (not vertical)
+	public ForceMode forceMode;
+	public Vector3 dir;
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void FixedUpdate(){
+		Vector3 camAngle = new Vector3(0, camera.transform.eulerAngles.y, 0);
+		Quaternion quat = Quaternion.Euler(camAngle);
+		Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0 ,Input.GetAxisRaw("Vertical")).normalized;
+		//Input axis is now corrected for camera rotation
+		input = quat * input;
+
+		//Apply as force or direct acceleration change
+		rb.AddForce(input * acceleration, forceMode);
+
+		//Cap max horizontal speed;
+		Vector3 hVel = rb.velocity;
+		hVel.y = 0;
+		hVel = hVel.sqrMagnitude > maxSpeed * maxSpeed ? hVel.normalized * maxSpeed : hVel;
+		hVel.y =  rb.velocity.y;
+		rb.velocity = hVel;
+
+	}
+}
