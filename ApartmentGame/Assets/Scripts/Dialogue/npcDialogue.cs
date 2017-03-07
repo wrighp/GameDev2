3269c3186b50589d71ce109bfc4739d00822a3fe
Ferrohound <ferrohound@gmail.com>
2017-03-07 14:29:52 -0500

    Dialogue triggers
    Still need to modify dialogue progression

--- ApartmentGame/Assets/Scripts/Dialogue/npcDialogue.cs
+++ ApartmentGame/Assets/Scripts/Dialogue/npcDialogue.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 using UnityEngine.UI;
 
+//Needs to have a canvas in the scene (doesn't have to be attached to camera)
+
 public class npcDialogue : MonoBehaviour {
 	
+	//the dialogue tree that's being referenced
 	Dialogue dialogue;
 	
+	//the dialogue box info
 	GameObject Name;
 	GameObject nodeText;
 	GameObject option1;
@@ -15,11 +19,26 @@ public class npcDialogue : MonoBehaviour {
 	GameObject option3;
 	GameObject exit;
 	
+	//cameras to use
+	public Camera mainCamera;
+	public Camera dialogueCamera;
+	public List<GameObject> speakers;
+	
+	//coroutine stuff
+	private IEnumerator runCoroutine;
+	private IEnumerator displayCoroutine;
+	
+	//node selection stuff
 	private int select = -2;
 	int nodeID = -1;
 	
 	public string Path;
 	public GameObject dialogueWindow;
+	
+	//Auto would cause the dialogue to run once the player enters the
+	//trigger area
+	public bool auto = false;
+	private bool running = false;
 	
 	// Use this for initialization
 	void Start () {
@@ -52,12 +71,22 @@ public class npcDialogue : MonoBehaviour {
 		
 		//to test
 		//move this runDialogue to a different function later
-		runDialogue();
+		//runDialogue();
 		
 	}
 	
 	public void runDialogue(){
-		StartCoroutine(run());
+		runCoroutine = run();
+		StartCoroutine(runCoroutine);
+	}
+	
+	public void loadDialogue(string newPath){
+		Path = newPath;
+		
+		//load the dialogue from the given path
+		dialogue = Dialogue.Load(Path);
+		select = -2;
+		nodeID = -1;
 	}
 	
 	//method for selecting the next dialogue node to load
@@ -65,35 +94,14 @@ public class npcDialogue : MonoBehaviour {
 		select = x;
 	}
 	
-	//coroutine for displaying the text, add something to allow the player to skip
-	//forward later
-	//handle text wrapping too
-	private IEnumerator DisplayText(string displayText){
-		int strLen = displayText.Length;
-		int index = 0;
-		
-		nodeText.GetComponent<Text>().text = "";
-		
-		while(true){
-			nodeText.GetComponent<Text>().text += displayText[index];
-			index++;
-			
-			if(index<strLen){
-				//play a sound potentially
-				//wait for a moment before adding next character
-				yield return new WaitForSeconds(0.05f);
-			}
-			else{
-				break;
-			}
-		}
-	}
-	
 	//update the text
 	private void updateText(Node node){
 		Name.GetComponent<Text>().text = node._name;
+		if(displayCoroutine!=null)
+			StopCoroutine(displayCoroutine);
 		
-		StartCoroutine(DisplayText(node._text));
+		displayCoroutine = DisplayText(node._text);
+		StartCoroutine(displayCoroutine);
 		
 		//nodeText.GetComponent<Text>().text = node._text;
 		
@@ -150,12 +158,64 @@ public class npcDialogue : MonoBehaviour {
 			dialogue._next = dialogue._nodes[nodeID]._reset;
 			nodeID = select;
 		}
+		running = false;
+		mainCamera.gameObject.SetActive(true);
+			dialogueCamera.gameObject.SetActive(false);
 		dialogueWindow.SetActive(false); 
+	}
+	
+	//coroutine for displaying the text, add something to allow the player to skip
+	//forward later
+	//handle text wrapping too
+	//also, break out of coroutine when the player advances
+	private IEnumerator DisplayText(string displayText){
+		int strLen = displayText.Length;
+		int index = 0;
+		
+		nodeText.GetComponent<Text>().text = "";
+		
+		while(true){
+			nodeText.GetComponent<Text>().text += displayText[index];
+			index++;
+			
+			if(index<strLen){
+				//play a sound potentially
+				//wait for a moment before adding next character
+				yield return new WaitForSeconds(0.05f);
+			}
+			else{
+				break;
+			}
+		}
 	}
 	
 	//shake the dialogue box, maybe play a noise
 	void shake(){
 		
+	}
+	
+	void OnTriggerEnter(Collider col){
+		if(col.tag == "Player" && auto && !running){
+			//run the dialogue
+			mainCamera.gameObject.SetActive(false);
+			dialogueCamera.gameObject.SetActive(true);
+			running = true;
+			Debug.Log("Welcome to dialogue!");
+		}
+	}
+	
+	void OnTriggerStay(Collider col){
+		if(col.tag!="Player" && !auto)
+			return;
+		
+		//if the player presses "interact"/fire1, disable player movement and
+		//run the dialogue tree
+		if (Input.GetButton("Fire1") && !running){
+			mainCamera.gameObject.SetActive(false);
+			dialogueCamera.gameObject.SetActive(true);
+			running = true;
+			runDialogue();
+		}
 	}
 	
 }
