9f45fc457c29aa97d320d2058ff7e67a05afcc62
Ferrohound <ferrohound@gmail.com>
2017-04-12 11:57:33 -0400

    Broken AF

--- ApartmentGame/Assets/Scripts/Dialogue/triggers/holdingTrigger.cs
+++ ApartmentGame/Assets/Scripts/Dialogue/triggers/holdingTrigger.cs
@@ -6,20 +6,23 @@ public class holdingTrigger : MonoBehaviour {
 	
 	public string thing;
 	public npcDialogue speaker;
+	public string tableVal;
 	public int targetNode;
 	public int defaultNode;
+	public GameObject target;
 	
 	void onTriggerStay(Collider col)
 	{
 		if(col.tag!="Player")
 			return;
-		
+		Debug.Log(Vector3.Distance(target.transform.position, transform.position));
 		//set the dialogue node to the target node
-		if(col.GetComponent<PlayerInteraction>().IsHolding(thing))
+		if(/*col.GetComponent<PlayerInteraction>().IsHolding(thing)*/ 
+		Vector3.Distance(target.transform.position, transform.position) < 5&& !npcDialogue.tasks[tableVal])
 		{
 			speaker.setReset(targetNode);
 		}
-		else
+		else if (!npcDialogue.tasks[tableVal] && Vector3.Distance(target.transform.position, transform.position) > 5)
 		{
 			speaker.setReset(defaultNode);
 		}
