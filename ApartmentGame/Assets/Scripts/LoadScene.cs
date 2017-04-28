48ac1bc7e616ccbf2018a552dc9117eec47a576d
Ferrohound <ferrohound@gmail.com>
2017-04-28 14:45:55 -0400

    Room number popup

--- ApartmentGame/Assets/Scripts/LoadScene.cs
+++ ApartmentGame/Assets/Scripts/LoadScene.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoadScene : MonoBehaviour {
 	
 	public Camera transition;
 	//public npcDialogue dummy;
 	public string scene;
+	public string room;
 	public int doorID = 1;
 	public static Dictionary<int, Transform> doors;
+	public GameObject doorSign;
 	
 	//set the door ID in the hash table, there must be a door with ID 0
 	void Awake()
@@ -18,11 +21,18 @@ public class LoadScene : MonoBehaviour {
 		
 		if(transform.childCount > 0)
 			doors[doorID] = this.gameObject.transform.GetChild(0);
+		
+		//doorSign = GameObject.Find("RoomNumber");
+		if(doorSign != null)
+			doorSign.SetActive(false);
 	}
 	
 	void OnTriggerStay(Collider col){
 		if(col.tag!="Player")
 			return;
+		
+		doorSign.SetActive(true);
+		doorSign.transform.Find("[roomText]").gameObject.GetComponent<Text>().text = room;
 
 		Vector3 p4 = col.transform.TransformDirection(Vector3.forward);
 		float PDotN = Vector3.Dot(p4, transform.position - col.transform.position);
@@ -30,7 +40,8 @@ public class LoadScene : MonoBehaviour {
 		if(Input.GetButtonDown("Fire1")
 			/*&& (PDotN>0.25|| Vector3.Distance(col.transform.position, transform.position) < 2)*/)
 		{
-			Debug.Log("Changing Scenes");
+			doorSign.SetActive(false);
+			//Debug.Log("Changing Scenes");
 			npcDialogue.saveState();
 			ProgressManager.doorID = doorID;
 			//preserve.Instance.transitions+=1;
@@ -38,6 +49,12 @@ public class LoadScene : MonoBehaviour {
 			SceneTransition.setScene(scene);
 			transition.GetComponent<SceneTransition>().play = true;
 		}
+	}
+	
+	//set the door number to inactive
+	void OnTriggerExit(Collider col)
+	{
+		doorSign.SetActive(false);
 	}
 	public void Load()
 	{
