a036c0a3a7500f4bae07fcefb3bb3b2eca3bd3a5
Janice Ho <janicehony@gmail.com>
2017-04-28 08:23:30 -0400

    Carrot & Carrot popup

--- ApartmentGame/Assets/Scripts/CarrotPop.cs
+++ ApartmentGame/Assets/Scripts/CarrotPop.cs
@@ -4,23 +4,31 @@ using UnityEngine;
 
 public class CarrotPop : MonoBehaviour {
 
+	Rigidbody rb;
 	private Animator animator;
+	private tmpItem itemscr;
 
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator> ();
+		rb = GetComponent<Rigidbody> ();
+		//rb.detectCollisions = false;
+		rb.constraints = RigidbodyConstraints.FreezeAll;
+		itemscr = GetComponent<tmpItem> ();
 	}
 	void OnTriggerStay (Collider col)
 	{
-
 		if (col.tag != "Player")
+			return;
 			//spawnballoon ();
 
 		if (Input.GetButton ("Fire1")) {
-			
-			print ("popthatshit");
-			animator.SetTrigger ("Pop");
-
+			Debug.Log ("PULL ME OUT");
+			rb.constraints = RigidbodyConstraints.None;
+			//rb.detectCollisions = true;
+			rb.AddForce (new Vector3 (0, 2f, 0));
+			itemscr.enabled = true;
+			this.enabled = false;
 		}
 	}
 }
