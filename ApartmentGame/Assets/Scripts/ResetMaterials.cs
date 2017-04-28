152f0917521494add460f59d7a09dbb82333d299
wrighp <wrightCoder@gmail.com>
2017-04-28 15:34:31 -0400

    ResetMaterials to be added to objects with whitenoise shader

---
+++ ApartmentGame/Assets/Scripts/ResetMaterials.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetMaterials : MonoBehaviour {
+	public static HashSet<string> resetAlready = new HashSet<string>();
+	// Use this for initialization
+	void Start () {
+		Renderer[] renderers = GetComponentsInChildren<Renderer>();
+		if(!resetAlready.Contains(gameObject.name)){
+			foreach (var renderer in renderers) {
+				if(renderer.material != null){
+					if(renderer.material.shader.name == "Whitenoise"){
+						//Reset values here
+						//renderer.material.set
+					}	
+				}
+			}
+			resetAlready.Add(gameObject.name);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
